13ab3d3cf84c89f4bd46bba3c901bf304ad4dcae
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-16 11:04:07 -0300

    Substitui o campo 'servico' por 'id_servico' no modelo Atendimento para melhor identificação do serviço realizado

--- classes/atendimento.py
+++ classes/atendimento.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 class Atendimento(BaseModel):
     id_pet: int = Field(..., description="ID do pet atendido")
     data_atendimento: str = Field(..., description="Data do atendimento")
-    servico: str = Field(..., description="Serviço realizado")
+    id_servico: int = Field(..., description="ID do serviço realizado")
     valor: float = Field(..., description="Valor do atendimento")
