c2bb61a9cd90e6ca00ffe240d22d9d26b6346fca
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-16 10:37:08 -0300

    Adiciona campos 'servico' e 'valor' ao modelo Atendimento

--- classes/atendimento.py
+++ classes/atendimento.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel, Field
 class Atendimento(BaseModel):
     id_pet: int = Field(..., description="ID do pet atendido")
     data_atendimento: str = Field(..., description="Data do atendimento")
-    descricao: str = Field(..., description="Descrição do atendimento")
+    servico: str = Field(..., description="Serviço realizado")
+    valor: float = Field(..., description="Valor do atendimento")
