c852f0743c653666934679539f6b04c06e581cf8
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-16 10:07:40 -0300

    Implementa funções para listar, obter, atualizar e deletar serviços na API

--- routers/servico.py
+++ routers/servico.py
@@ -32,4 +32,72 @@ def create_servico(servico: Servico):
    
     return {"message": "Serviço criado com sucesso!"}
 
+@router.get("")
+def get_servicos():  
+    try:
+        with engine.connect() as conn:
+            sql = """SELECT * FROM servico"""
+            result = conn.execute(text(sql))
+            servicos = [dict(row._mapping) for row in result]
+            return servicos
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"message": "Serviços listados com sucesso!"}
+
+@router.get("/{servico_id}")
+def get_servico(servico_id: int):   
+    try:
+        with engine.connect() as conn:
+            sql = """SELECT * FROM servico WHERE id_servico = :servico_id"""
+            result = conn.execute(text(sql), {"servico_id": servico_id})
+            servico = result.fetchone()
+            if servico:
+                return dict(servico)
+            else:
+                return {"message": "Serviço não encontrado"}
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"message": "Serviço listado com sucesso!"}
+
+@router.put("/{servico_id}")
+def update_servico(servico_id: int, servico: Servico):
+    try:
+        with engine.connect() as conn:
+            sql = """UPDATE servico 
+                    SET tipo_servico = :tipo_servico, valor = :valor 
+                    WHERE id_servico = :servico_id"""
+
+            dados = {
+                "tipo_servico": servico.tipo_servico,
+                "valor": servico.valor,
+                "servico_id": servico_id
+            }
+
+            result = conn.execute(text(sql), dados)
+            conn.commit()
+
+            if result.rowcount == 0:
+                return {"message": "Serviço não encontrado"}
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"message": "Serviço atualizado com sucesso!"}
+
+@router.delete("/{servico_id}")
+def delete_servico(servico_id: int):
+    try:
+        with engine.connect() as conn:
+            sql = """DELETE FROM servico WHERE id_servico = :servico_id"""
+            result = conn.execute(text(sql), {"servico_id": servico_id})
+            conn.commit()
+
+            if result.rowcount == 0:
+                return {"message": "Serviço não encontrado"}
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"message": "Serviço deletado com sucesso!"} 
+
     
