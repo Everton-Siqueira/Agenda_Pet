e6301bdafb20e1c6f7117e6ecb7b345fd1983bbc
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-18 17:11:21 -0300

    Atualiza consultas SQL para utilizar o campo 'id' em vez de 'id_servico' nas funções de listagem, atualização e deleção de serviços

--- routers/servico.py
+++ routers/servico.py
@@ -43,13 +43,12 @@ def get_servicos():
     except Exception as e:
         return {"error": str(e)}
 
-    return {"message": "Serviços listados com sucesso!"}
-
+    
 @router.get("/{servico_id}")
 def get_servico(servico_id: int):   
     try:
         with engine.connect() as conn:
-            sql = """SELECT * FROM servico WHERE id_servico = :servico_id"""
+            sql = """SELECT * FROM servico WHERE id = :servico_id"""
             result = conn.execute(text(sql), {"servico_id": servico_id})
             servico = result.fetchone()
             if servico:
@@ -59,15 +58,14 @@ def get_servico(servico_id: int):
     except Exception as e:
         return {"error": str(e)}
 
-    return {"message": "Serviço listado com sucesso!"}
-
+    
 @router.put("/{servico_id}")
 def update_servico(servico_id: int, servico: Servico):
     try:
         with engine.begin() as conn:
             sql = """UPDATE servico 
                     SET tipo_servico = :tipo_servico, valor = :valor 
-                    WHERE id_servico = :servico_id"""
+                    WHERE id = :servico_id"""
 
             dados = {
                 "tipo_servico": servico.tipo_servico,
@@ -76,7 +74,7 @@ def update_servico(servico_id: int, servico: Servico):
             }
 
             result = conn.execute(text(sql), dados)
-            conn.execute()
+           
 
             if result.rowcount == 0:
                 return {"message": "Serviço não encontrado"}
@@ -89,7 +87,7 @@ def update_servico(servico_id: int, servico: Servico):
 def delete_servico(servico_id: int):
     try:
         with engine.begin() as conn:
-            sql = """DELETE FROM servico WHERE id_servico = :servico_id"""
+            sql = """DELETE FROM servico WHERE id = :servico_id"""
             result = conn.execute(text(sql), {"servico_id": servico_id})
             
 
