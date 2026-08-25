23e83d3b732c698a9dd8ce5bc017630a3ea55cf5
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-24 21:24:48 -0300

    Refatora o modelo Atendimento para utilizar tipos de dados apropriados e adiciona validação para o horário de atendimento

--- classes/atendimento.py
+++ classes/atendimento.py
@@ -1,8 +1,31 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+from datetime import date, time
+from decimal import Decimal
 
 class Atendimento(BaseModel):
-    id_pet: int = Field(..., description="ID do pet atendido")
-    data_atendimento: str = Field(..., description="Data do atendimento")
-    horario_atendimento: str = Field(..., description="Hora do atendimento")
-    id_servico: int = Field(..., description="ID do serviço realizado")
-    valor: float = Field(..., description="Valor do atendimento")
+    id_pet: int = Field(...,
+    gt= 0,
+    description="ID do pet atendido")
+
+    data_atendimento: date = Field(...,
+    description="Data do atendimento")
+
+    horario_atendimento: time = Field(...,
+    description="Hora do atendimento")
+    
+    id_servico: int = Field(...,
+    gt= 0,
+    description="ID do serviço realizado")
+
+    valor: Decimal = Field(...,
+    gt=0,
+    le=999.99,
+    decimal_places=2,
+    description="Valor do atendimento")
+
+    @field_validator('horario_atendimento')
+    def validar_horario_atendimento(cls, horario_atendimento):
+        if horario_atendimento < time(9, 0) or horario_atendimento > time(18, 0):
+            raise ValueError("O horário de atendimento deve estar entre 09:00 e 18:00.")
+        return horario_atendimento
+        
